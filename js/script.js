7cc61622e62c0fd5c35528a1328b3191c4127fbb
GiandoSaba <giandosaba997@gmail.com>
2021-12-01 15:17:15 +0100

    end

--- js/script.js
+++ js/script.js
@@ -5,3 +5,49 @@
 // con difficoltà 3 => tra 1 e 49
 
 // Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.
+
+const playButton = document.getElementById('play-button');
+const grid = document.getElementById('grid');
+
+playButton.addEventListener('click', function () {
+    grid.innerHTML = '';
+    const selectValue = document.getElementById('difficulty-select').value;
+    let row = 0;
+    let col = 0;
+    switch (selectValue) {
+        case 'easy':
+            row = 10;
+            col = 10;
+            break;
+        case 'medium':
+            row = 9;
+            col = 9;
+            break;
+        case 'hard':
+            row = 7;
+            col = 7;
+            break;
+    }
+
+    const numberSquare = row * col;
+
+    for (let i = 0; i < numberSquare; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        const width = `calc(100% / ${col})`;
+        const height = `calc(100% / ${row})`;
+        square.append(i+1);
+        square.style.width = width;
+        square.style.height = height;
+        grid.append(square);
+        
+
+        square.addEventListener('click', function() {
+            this.classList.add('clicked');
+        });
+
+
+        
+    }
+
+});
